658083c7ae27b21ee79e2abcb62089f590216600
suryyyansh <suryansh.arya5472@gmail.com>
2024-07-18 14:37:23 +0530

    made improvements. still only supports google

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var { Readability } = require('@mozilla/readability');
+var { Readability, isProbablyReaderable } = require('@mozilla/readability');
 var { JSDOM } = require('jsdom');  
 var https = require('node:https');
 
@@ -15,13 +15,18 @@ const port = 3000;
 
 let defaultLogLevel = "debug"
 
+//disabling css errors for JSDOM. Has the side effect of also disabling every other kind of error. Comment out when debugging.
+console.error = () => {}
+
 // Define a route handler for the root URL
 app.post('/search', (req, res) => {
   const body = req.body
   const term = body.term;
   const engine = body.engine;
-  const maxSearchResults = body.maxSearchResults ? body.maxSearchResults : 5;
+  const requiredSearchResults = body.maxSearchResults ? body.maxSearchResults : 5;
+  var currentSearchResults = requiredSearchResults;
   var engineSearchUrl;
+  var currentSiteList = [];
 
   var searchHandlerGoogle = (url) => {
     https.get(url, (httpsRes) => {
@@ -56,18 +61,29 @@ app.post('/search', (req, res) => {
               for (x of resultUrls) {
                 if(!x.closest('a') || !x.closest('a').getAttribute('href')) continue;
                 let currentNodeHref = x.closest('a').getAttribute('href') 
-                if (i++ >= maxSearchResults) break;
+                if (i++ >= currentSearchResults) break;
                 
                 let refinedCurrentNodeHref = decodeURIComponent(
-                  currentNodeHref.substring(7, currentNodeHref.indexOf("&sa")))
-                
+                  currentNodeHref.substring(currentNodeHref.indexOf("http"), currentNodeHref.indexOf("&sa")))
+                if(!refinedCurrentNodeHref.startsWith("http"))
+                  continue;
+
                 log("debug", `current node being processed: ${refinedCurrentNodeHref}`);
-                promises.push(parseWithReadability(refinedCurrentNodeHref));
+                promises.push(parseWithReadability(refinedCurrentNodeHref, currentSiteList));
               }
 
               Promise.all(promises).then(parsedSiteList => {
-                res.send(parsedSiteList);
-                log("info", "Query responded")
+                currentSiteList = parsedSiteList;
+                
+                if(parsedSiteList.filter(Boolean).length < requiredSearchResults) {
+                  log("info", "Result criteria unmet: increasing search count: ")
+                  currentSearchResults++;
+                  searchHandlerGoogle(url);
+
+                } else {
+                  res.send(parsedSiteList.filter(Boolean))
+                  log("info", "Query responded")
+                }
               });
             });
           });
@@ -112,26 +128,34 @@ const logLevels = (...msg) => {
   };
   return (style) => levels[style];
 };
-
 function log(logLevel=defaultLogLevel, ...msg) {
   console.log(logLevels()(logLevel + 'Marker') + " " + logLevels(msg)(logLevel))
 }
 
-async function parseWithReadability(url) {
+async function parseWithReadability(url, parsedSiteList=[]) {
+  //avoid parsing the same site twice
+  
+
+  parsedSiteList.forEach((site) => {
+    if(site && site.url == url) 
+      return site
+  }); 
 
   log("debug", `parsing with readability: ${url}`)
   
-  let dom = await JSDOM.fromURL(url);
+  try {
+    var dom = await JSDOM.fromURL(url);
+  } catch (e) {
+    log("error", "JSDOM.fromURL(url)")
+  }
+  if(!isProbablyReaderable(dom.window.document)) return null;
   let article = new Readability(dom.window.document).parse();
 
   if(!article || !article.textContent) {
     log("error", `failed to parse article "${url}".`)
-    return;
+    return null;
   }
-
   log("info", `article "${url}" parsed`)
-  
-  // we consider div contents to be readable
 
   let readableDom = new JSDOM(article.content
     .replaceAll("\t"," ")
@@ -140,28 +164,33 @@ async function parseWithReadability(url) {
     .trim()
   )
 
-  let readableDivs = readableDom.window.document.querySelectorAll('div');
+  let readableDivs = readableDom.window.document.querySelectorAll('p');
+
   let readableDivContents = []
   for (div of readableDivs) {
     let divArticle = new Readability(
       new JSDOM(div.innerHTML).window.document
     ).parse();
 
-    if (divArticle && divArticle.textContent) {
+    if (divArticle && divArticle.textContent && divArticle.textContent.trim().length >= 200) {
       readableDivContents.push(divArticle.textContent.slice(0,1000))
     }
   }
 
+  textContent = readableDivContents.join("\ny_url = 'https://www.instagram.").slice(0,1000)
+  
+  if(textContent.length < 200)
+    return null
+
   return { 
     siteName: article.siteName,
     url: url,
-    textContent: readableDivContents.join("\n").slice(0,4000) // TODO: a rough limit for content supplied 
+    textContent: textContent // TODO: a rough limit for content supplied 
   };
 }
 
 // Start the server and listen on the specified port
 app.listen(port, () => {
-  console.log("\n");
-  log("debug", `Server running at http://localhost:${port}/`); // Log server start info
+  log("debug", `Server running at http://localhost:${port}/\n`); // Log server start info
 });
 
